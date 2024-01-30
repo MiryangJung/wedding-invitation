f7139e1fc3abb993abc437bd3d411158c339f1f5
MiryangJung <miryang.dev@gmail.com>
2024-01-31 02:43:55 +0900

    주석 해제

--- app/components/spline-love.tsx
+++ app/components/spline-love.tsx
@@ -4,7 +4,7 @@ import { container } from "./spline-love.css";
 export default function SplineLove() {
   return (
     <div className={container}>
-      {/* <Spline scene="https://prod.spline.design/5ZOC7Jegz859We9z/scene.splinecode" /> */}
+      <Spline scene="https://prod.spline.design/5ZOC7Jegz859We9z/scene.splinecode" />
     </div>
   );
 }
